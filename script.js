28639af01c159aefa4e598039d56b6d04c793e34
DevshriS <devshrivsuryawanshi@gmail.com>
2023-04-14 16:03:28 +0530

    final commit2

--- script.js
+++ script.js
@@ -1,79 +1,105 @@
-obj = [
+const quizData = [
     {
-      question: "Which language runs in a web browser?",
-      a: "Java",
-      b: "C",
-      c: "Python",
-      d: "JavaScript",
-      correct: "d",
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
     },
     {
-      question: "What does CSS stand for?",
-      a: "Central Style Sheets",
-      b: "Cascading Style Sheets",
-      c: "Cascading Simple Sheets",
-      d: "Cars SUVs Sailboats",
-      correct: "b",
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
     },
     {
-      question: "What does HTML stand for?",
-      a: "Hypertext Markup Language",
-      b: "Hypertext Markdown Language",
-      c: "Hyperloop Machine Language",
-      d: "Helicopters Terminals Motorboats Lamborginis",
-      correct: "a",
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
     },
     {
-      question: "What year was JavaScript launched?",
-      a: "1996",
-      b: "1995",
-      c: "1994",
-      d: "none of the above",
-      correct: "b",
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
     },
-  ];
-  let submit = document.getElementById("submit");
-  let question = document.getElementById("question");
-  let a_text = document.getElementById("a_text");
-  let b_text = document.getElementById("b_text");
-  let c_text = document.getElementById("c_text");
-  let d_text = document.getElementById("d_text");
-  // let a = document.getElementById("a");
-  // let b = document.getElementById("b");
-  // let c = document.getElementById("c");
-  // let d = document.getElementById("d");
-  let marks = 0;
-  var questionTracker = 0;
-  submit.addEventListener("click", () => {
-    var userAnswer = document.querySelector('input[name="option"]:checked');
-    if (userAnswer) {
-      if (userAnswer.id == obj[questionTracker].correct) {
-        marks++;
-      }
-      questionTracker++;
-      userAnswer.checked = false;
-      if (obj.length > questionTracker) {
-        displayQuestion(questionTracker);
-      } else {
-        question.innerHTML = `You ansered ${marks}/4 questions correctly`;
-        let option_box = document.getElementById("option-box");
-        option_box.style.cssText = "display: none;";
-      }
+];
+
+// const questions = [{},{},{},{}]
+
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
+
+const submitBtn = document.getElementById("submit")
+
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+
+let score = 0
+
+let currentQuestion = 0
+
+function loadQuestion(){
+
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuestion]
+
+    questionEl.innerText = currentQuizData.question
+
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers(){
+    answerEls.forEach(answerEl=> answerEl.checked=false)
+}
+
+function getSelected(){
+    let answer;
+
+    answerEls.forEach(answerEl=>{
+        if(answerEl.checked){
+            answer = answerEl.id
+        }
+    })
+    console.log(answer)
+    return answer
+}
+
+loadQuestion()
+
+submitBtn.addEventListener("click",()=>{
+
+    const answer = getSelected()
+
+    if(answer){
+        if(answer === quizData[currentQuestion].correct){
+            score++;
+        }
+        currentQuestion++
+
+        if(currentQuestion<quizData.length){
+            loadQuestion()
+        }else{
+            quiz.innerHTML = `
+            <h2>
+                You answered ${score}/${quizData.length} questions correctly
+            </h2>
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
     }
-  });
-  function displayQuestion(questionTracker) {
-    //it use for showing the qustion
-    question.innerHTML = obj[questionTracker].question;
-    a_text.innerHTML = obj[questionTracker].a;
-    b_text.innerHTML = obj[questionTracker].b;
-    c_text.innerHTML = obj[questionTracker].c;
-    d_text.innerHTML = obj[questionTracker].d;
-  }
-  
-  let quz = document.getElementById("quiz");
-  
-  window.addEventListener("load", () => {
-    quz.style.cssText = "display: block;";
-  
-    displayQuestion(questionTracker);
-  });
+})
